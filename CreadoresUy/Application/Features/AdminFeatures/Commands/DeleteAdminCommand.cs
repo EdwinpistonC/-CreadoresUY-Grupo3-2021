e882455c7863612c11f8cec477ff460385ca0312
Edwin Matias Piston Carrizo <36250815+EdwinpistonC@users.noreply.github.com>
2021-10-29 20:54:33 -0300

    fix problema

---
+++ CreadoresUy/Application/Features/AdminFeatures/Commands/DeleteAdminCommand.cs
@@ -0,0 +1,35 @@
+﻿using Application.Interface;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.AdminFeatures.Commands
+{
+
+    
+    public class DeleteAdminCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public class DeleteAdminCommandHandler : IRequestHandler<DeleteAdminCommand, int>
+        {
+            private readonly ICreadoresUyDbContext _context;
+            public DeleteAdminCommandHandler(ICreadoresUyDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<int> Handle(DeleteAdminCommand command, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.Where(a => a.Id == command.Id).FirstOrDefaultAsync();
+                if (user == null) return default;
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+                return user.Id;
+            }
+        }
+    }
+}
