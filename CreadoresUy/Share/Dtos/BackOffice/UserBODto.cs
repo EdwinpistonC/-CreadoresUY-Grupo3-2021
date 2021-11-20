9034205b92822a14ff6e6e324e296d4f61a5ce6e
Edwin Matias Piston Carrizo <36250815+EdwinpistonC@users.noreply.github.com>
2021-11-20 17:01:38 -0300

    fix

--- CreadoresUy/Share/Dtos/BackOffice/UserBODto.cs
+++ CreadoresUy/Share/Dtos/BackOffice/UserBODto.cs
@@ -27,7 +27,7 @@ namespace Share.Entities
             if (ImgProfile == null) ImgProfile ="";
             if (Created == null) Created = DateTime.MinValue;
             if (LasLogin == null) LasLogin = DateTime.MinValue;
-            if (CreatorId <=0) CreatorId = 0;
+            if (CreatorId <=0 || CreatorId == null) CreatorId = 0;
             Password = "";
         }
 
