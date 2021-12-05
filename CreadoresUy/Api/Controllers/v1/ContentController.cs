a254c9c1967c4a7229dbe510fc79a50923ea8cf5
lmartin23 <lmaartin997@gmail.com>
2021-12-05 17:56:29 -0300

    Crud Contenido Completo

--- CreadoresUy/Api/Controllers/v1/ContentController.cs
+++ CreadoresUy/Api/Controllers/v1/ContentController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.ContentFeature.Commands;
+using Application.Features.ContentFeature.Queries;
 using Application.Features.CreatorFeatures.Queries;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +23,30 @@ namespace Api.Controllers.v1
         public async Task<IActionResult> CreateNewContent(CreateNewContentCommand command)
         {
             return Ok(await Mediator.Send(command));
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("GetContentDraft")]
+        public async Task<IActionResult> GetContentDraftQuery(string nickname)
+        {
+            return Ok(await Mediator.Send(new GetContentDraftQuery { Nickname = nickname }));
+        }
+
+        [AllowAnonymous]
+        [HttpPut]
+        [Route("UpdateContent")]
+        public async Task<IActionResult> Update(UpdateContentCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpDelete]
+        [AllowAnonymous]
+        [Route("DeleteContent")]
+        public async Task<IActionResult> Delete(int idCre, int idCont)
+        {
+            return Ok(await Mediator.Send(new DeleteContentCommand { IdCreator = idCre, IdContent = idCont }));
         }
 
         [AllowAnonymous]
