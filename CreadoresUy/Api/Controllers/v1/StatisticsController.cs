31e854ee6e2d1cf56344cf27aaeabef540db62f2
Edwin Matias Piston Carrizo <36250815+EdwinpistonC@users.noreply.github.com>
2021-12-06 23:31:14 -0300

    estadisticas

---
+++ CreadoresUy/Api/Controllers/v1/StatisticsController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.StatisticsFeaturesBO.Queries;
+using Application.Features.UserFeatures.Commands;
+using Application.Features.UserFeatures.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Share.Dtos;
+using System.Threading.Tasks;
+
+namespace Api.Controllers.v1
+{
+
+    [ApiVersion("1.0")]
+    public class StatisticsController : BaseApiController
+    {
+
+        [HttpGet("GetFinances")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetFinances()
+        {
+            return Ok(await Mediator.Send(new GetFinancesQuery()));
+
+        }
+
+
+
+    }
+
+}
---
+++ CreadoresUy/Share/Dtos/BackOffice/StatisticsBODto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Share.Entities
+{
+    /*
+     * Name y email no se pueden repetir en la base
+     */
+    public class StatisticsBODto<T>
+    {
+        public T XValue { get; set; }
+        public Double YValue { get; set; }
+
+    }
+}
