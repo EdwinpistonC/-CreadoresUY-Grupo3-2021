d21169b5c06b5561d4a39c3126ba0bb2c975fa56
lmartin23 <lmaartin997@gmail.com>
2021-12-09 11:51:43 -0300

    Estadisticas

---
+++ CreadoresUy/Application/Features/StatisticsFeaturesBO/Queries/GetUnsubscribersQuery.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interface;
+using MediatR;
+using Share.Dtos;
+using Share.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.StatisticsFeaturesBO.Queries
+{
+    public class GetUnsubscribersQuery  : IRequest<Response<List<StatisticsBODto<int>>>>
+    {
+        public class GetUnsubscribersQueryHandler : IRequestHandler<GetUnsubscribersQuery, Response<List<StatisticsBODto<int>>>>
+        {
+            private readonly ICreadoresUyDbContext _context;
+
+            public GetUnsubscribersQueryHandler(ICreadoresUyDbContext context)
+            {
+                _context = context;
+            }
+
+            public async  Task<Response<List<StatisticsBODto<int>>>> Handle(GetUnsubscribersQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
