535ef3018555cfa5b4eabed978ffead19a5d9f43
lmartin23 <lmaartin997@gmail.com>
2021-12-13 10:36:54 -0300

    cambios para el suscribirse

--- CreadoresUy/Share/Dtos/BackOffice/PaymentBODto.cs
+++ CreadoresUy/Share/Dtos/BackOffice/PaymentBODto.cs
@@ -1,20 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Share.Entities
+namespace Share.Dtos.BackOffice
 {
     /*
      * Name y email no se pueden repetir en la base
      */
     public class PaymentBODto
     {
-        public int? Id { get; set; }
-        public bool? Nickname { get; set; }
-        public string UserName { get; set; }
-        public string Plan { get; set; }
-        public float Price { get; set; }
-        public string Amount { get; set; }
-        public string Date { get; set; }
+        public int IdCreator { get; set; }
+        public string Nickname { get; set; }
+        public double Amount { get; set; }
+        public string Month { get; set; }
 
     }
 
