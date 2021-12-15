2203d6ae3093bc9fa5c315b2c491868a6d5ecc51
lmartin23 <lmaartin997@gmail.com>
2021-12-15 18:14:26 -0300

    Actualizacion del validador

--- CreadoresUy/Application/Features/Validators/PlansAndBenefitsValidator.cs
+++ CreadoresUy/Application/Features/Validators/PlansAndBenefitsValidator.cs
@@ -21,8 +21,6 @@ namespace Application.Features.Validators
             RuleFor(x => x.Name).NotEmpty().WithMessage("{PropertyName} No puede ser vacio")
                 .Must(ExistePlan).WithMessage("{PropertyName} = {PropertyValue} -> no puede ser ingresado porque ya existe un plan con el mismo nombre");
             RuleFor(x => x.Price).NotEmpty().WithMessage("{PropertyName} No puede ser vacio");
-            RuleFor(x => x.WelcomeVideoLink).NotEmpty().WithMessage("{PropertyName} No puede ser vacio");
-            RuleFor(x => x.SubscriptionMsg).NotEmpty().WithMessage("{PropertyName} No puede ser vacio");
             RuleFor(x => x.Benefits).Must(Validar).WithMessage("{PropertyName} Debe contener al menos un Beneficio");
         }
         public bool Validar(ICollection<string> dato)
